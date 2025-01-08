84dccc4be4d0819f005b383fae2eb594fd329520
littlejohnnyzq <littlejohnnyzq@icloud.com>
2025-01-08 14:18:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ async function fetchPluginData(previousData, attempt = 1) {
         const url1 = 'https://www.figma.com/community/search?resource_type=plugins&sort_by=relevancy&query=canned&editor_type=all&price=all&creators=all';
         const url2 = 'https://www.figma.com/community/tag/code%20snippets/plugins';
         const url4 = 'https://www.figma.com/community/search?resource_type=plugins&sort_by=relevancy&query=zeplin+team+across&editor_type=all&price=all&creators=all';
-        const url3 = 'https://www.figma.com/community/search?resource_type=plugins&sort_by=relevancy&query=lanhu&editor_type=all&price=all&creators=all';
+        const url3 = 'https://www.figma.com/community/search?resource_type=plugins&sort_by=relevancy&query=Lanhu+%C2%B7+%E8%93%9D%E6%B9%96&editor_type=all&price=all&creators=all';
         // Fetch data from the first URL
         const data1 = await fetchPageData(url1, previousData);
         const data2 = await fetchPageData(url2, previousData);
